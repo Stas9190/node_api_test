7e023657994742bf7f60631f26446039a84f22ed
Станислав Алексеевич Шульга <s.shulga@innopolis.local>
2022-07-21 13:55:19 +0300

    add methods

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ app.use(bodyParser.raw());
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('it works~');
+app.get('/test', (req, res) => {
+    console.log('get test');
+    res.sendStatus(200);
 });
 
 app.post('/test', (req, res) => {
-    console.log('test');
+    console.log('post test');
     try {
         const body = req.body;
         console.log(body);
@@ -22,6 +23,11 @@ app.post('/test', (req, res) => {
         console.error(e.messgae);
     }
 });
+
+app.put('/test', (req, res) => {
+    console.log('put test');
+    res.sendStatus(200);
+})
 
 app.listen(port, () => {
     console.log(`server listening on port ${port}`);
