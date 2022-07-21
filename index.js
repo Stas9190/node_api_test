6a749694cf13eb8253dc98085bef4aa3c6a61b32
Станислав Алексеевич Шульга <s.shulga@innopolis.local>
2022-07-21 11:36:01 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('it works~');
+});
+
+app.post('/test', (req, res) => {
+    try {
+        const body = req.body;
+        console.log(body);
+        res.sendStatus(200);
+    } catch (e) {
+        console.error(e.messgae);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});
