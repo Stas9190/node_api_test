1cd5226b60ba9d3b624e07043d9a6d13b93a9bb3
Станислав Алексеевич Шульга <s.shulga@innopolis.local>
2022-07-27 21:42:05 +0300

    change sent status

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/test', (req, res) => {
         const body = req.body;
         console.log(body);
         console.log('-----------------------------------');
-        res.status(401).send('Anaothorized');
+        res.status(200).send('Ok');
     } catch (e) {
         console.error(e.messgae);
     }
